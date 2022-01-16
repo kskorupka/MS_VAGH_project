477bb63c2d59951bced51968d3d618847c00a349
kskorupka <72651147+kskorupka@users.noreply.github.com>
2022-01-16 16:59:45 +0100

    adding_admin_and_tests

--- website/tests/tests.py
+++ website/tests/tests.py
@@ -1,15 +1,23 @@
 from unittest import TestCase
-from ..auxiliary_functions import get_bikes, get_scooters, get_skateboards
+from ..auxiliary_functions import get_bikes, get_scooters, get_skateboards, check_if_item_is_available
+from sqlalchemy import engine
+
 
 class testForUser(TestCase):
 
     def testGetBikes(self):
         from ..models import Location, Item
-        # items = list([1, 1, "Rower", 20, "niebieski"], [2, 2, "Hulajnoga", 4, "niebieski"],
-        #              [3, 1, 'Deskorolka', 4, "niebieski"], [4, 1, "Rower", 4, "niebieski"])
+
+        # items = [engine.Row(1, 1, 'Rower', 'niebieski'), engine.Row(2, 2, 'Hulajnoga', 'niebieski'),
+        #          engine.Row(3, 1, 'Deskorolka', 'niebieski'),
+        #          engine.Row(4, 1, 'Rower', 'niebieski')]
         #
-        # locations = list([1, "Lok1", "Lok1", "Lo1"],[1, "Lok2", "Lok2", "Lo2"])
+        # locations = [Location(1, 'Lok1', 'Lok1', 'Lo1'), Location(2, 'Lok2', 'Lok2', 'Lo2')]
         #
-        # bikes = get_bikes(items,locations)
+        # bikes = get_bikes(items, locations)
+        #
+        # correct_bikes = [[1, 1, "Rower", 20, "niebieski"], [4, 1, "Rower", 4, "niebieski"]]
+
+        # self.assertEqual(bikes, correct_bikes)
 
 
